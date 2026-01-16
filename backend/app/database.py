2548f1120817e0ace0658b0cdbfdc8e0cd6d7f85
camahoney <deathlesscourage@gmail.com>
2026-01-16 05:25:12 -0600

    Add files via upload

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlmodel import SQLModel, create_engine, Session
 from pathlib import Path
 
-DATABASE_URL = "sqlite:///./oee_app_v3.db"
+DATABASE_URL = "sqlite:///./oee_app_v4.db"
 
 engine = create_engine(DATABASE_URL, echo=False)
 
